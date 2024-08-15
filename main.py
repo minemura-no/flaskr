53bed60ccbb8c7dec51cedeac0f6663e559cfb45
nminebb <nobuo.minemura@gmail.com>
2024-08-15 13:26:58 +0900

    初回のコミット

---
+++ main.py
@@ -0,0 +1,12 @@
+from __init__ import app
+from flask import render_template
+@app.route('/')
+def index():
+    book = {
+        'title': 'はらぺこあおむし',
+        'price': 1200,
+        'arrival_day': '2020年7月12日'
+    }
+    return render_template(
+        'index.html'
+    )
